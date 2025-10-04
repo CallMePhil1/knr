421c6df5064bbb7fc1eb42430dc6af57ade8502a
CallMePhil <callmephil@github.com>
2025-10-03 21:20:06 -0400

    Change arc in NativeArray to non null and immutable, remove onArcUpdate

--- runtime/src/main/kotlin/com/github/callmephil/knr/runtime/typing/array/ByteNativeArray.kt
+++ runtime/src/main/kotlin/com/github/callmephil/knr/runtime/typing/array/ByteNativeArray.kt
@@ -4,17 +4,16 @@ import com.github.callmephil.knr.runtime.memory.ARC
 
 open class ByteNativeArray internal constructor(
     arc: ARC,
-    onArcUpdated: (() -> Unit)?
-) : NativeArray<Byte, ByteArray>(arc, onArcUpdated, Byte.SIZE_BYTES) {
+) : NativeArray<Byte, ByteArray>(arc, Byte.SIZE_BYTES) {
 
     override operator fun get(index: Int): Byte {
         checkBounds(index)
-        return arc!!.getByte(index.toLong())
+        return arc.getByte(index.toLong())
     }
     override fun get() = getByteArray()
     override fun getArray(): Array<Byte> {
-        val buffer = arc!!.asByteBuffer()
-        val arraySize = (arc!!.byteSize / typeByteSize).toInt()
+        val buffer = arc.asByteBuffer()
+        val arraySize = (arc.byteSize / typeByteSize).toInt()
 
         val array = Array<Byte>(arraySize) { 0 }
 
@@ -27,16 +26,16 @@ open class ByteNativeArray internal constructor(
 
     override operator fun set(index: Int, value: Byte) {
         checkBounds(index)
-        arc!!.setByte(index.toLong(), value)
+        arc.setByte(index.toLong(), value)
     }
     override fun set(value: ByteArray) = setByteArray(value)
     override fun set(value: Array<Byte>) {
         checkBounds(value.size)
-        value.forEachIndexed { idx, byte -> arc!!.setByte(idx.toLong(), byte) }
+        value.forEachIndexed { idx, byte -> arc.setByte(idx.toLong(), byte) }
     }
 }
 
-fun byteNativeArray(arc: ARC) = ByteNativeArray(arc, null)
+fun byteNativeArray(arc: ARC) = ByteNativeArray(arc)
 fun byteNativeArray(size: Long) = byteNativeArray(ARC.shared(size))
 fun byteNativeArray(array: ByteArray): ByteNativeArray {
     val arc = ARC.shared(array.size.toLong())
--- runtime/src/main/kotlin/com/github/callmephil/knr/runtime/typing/array/NativeArray.kt
+++ runtime/src/main/kotlin/com/github/callmephil/knr/runtime/typing/array/NativeArray.kt
@@ -3,27 +3,10 @@ package com.github.callmephil.knr.runtime.typing.array
 import com.github.callmephil.knr.runtime.memory.ARC
 
 abstract class NativeArray<T, C> internal constructor(
-    arc: ARC?,
-    private val onArcUpdated: (() -> Unit)?,
+    val arc: ARC,
     val typeByteSize: Int
 ) {
-    var arc: ARC? = arc
-        internal set(value) {
-            field = value
-            calculateSize()
-            onArcUpdated?.invoke()
-        }
-
-    var size: Int = 0
-        private set
-
-    init {
-        calculateSize()
-    }
-
-    private fun calculateSize() {
-        size = ((arc?.byteSize ?: 0) / typeByteSize).toInt()
-    }
+    val size: Int = (arc.byteSize / typeByteSize).toInt()
 
     protected fun checkBounds(target: Int) {
         if (size < target)
@@ -40,6 +23,6 @@ abstract class NativeArray<T, C> internal constructor(
     abstract fun set(value: Array<T>)
     fun setByteArray(value: ByteArray) {
         checkBounds(value.size)
-        arc!!.setBytes(value)
+        arc.setBytes(value)
     }
 }
